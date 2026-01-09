cca47db3a810faac361a75879a17dc7e5720795b
Yuvraj <yuvrajsoman@gmail.com>
2026-01-09 16:49:11 +0530

    created signin and signup page UI

---
+++ lib/Signup_screen.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(top:40),
+            child: CircleAvatar(
+              backgroundColor: const Color(0xFF2F3A4A),
+              radius: 40,
+              child: const Icon(
+                Icons.person,
+                size: 48,
+                color: Colors.cyanAccent,
+              ),  
+            )
+
+          ),
+
+          const SizedBox(height: 30),
+
+          Container(
+        width: 300,
+        margin: const EdgeInsets.symmetric(horizontal: 24),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: const Color(0xFF2F3A4A),
+          
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.6),
+              offset: const Offset(4, 4),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+
+        
+        child: Column(
+          children: [
+
+            //Name field
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              decoration: const InputDecoration(
+                labelText: "Name",
+                labelStyle: TextStyle(color: Colors.white70),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white30),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.cyanAccent),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 16),
+            // Enter Email field
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              decoration: const InputDecoration(
+                labelText: "Email",
+                labelStyle: TextStyle(color: Colors.white70),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white30),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.cyanAccent),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            // Set Password field
+            TextField(
+              obscureText: true,
+              style: const TextStyle(color: Colors.white),
+              decoration: const InputDecoration(
+                labelText: "Set Password",
+                labelStyle: TextStyle(color: Colors.white70),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white30),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.cyanAccent),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 24),
+
+            // Sign In button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {}, // UI only
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.cyanAccent,
+                  foregroundColor: Colors.black,
+                  padding: const EdgeInsets.symmetric(vertical: 14),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: const Text(
+                  "Sign Up",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 12),
+
+            MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: GestureDetector(
+                onTap: () {
+                  //forgot password logic
+                },
+                child: const Text(
+                  "Already a User? Sign In",
+                  style: TextStyle(
+                    color: Colors.cyanAccent,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+
+
+        ],
+      ),
+          )
+      ] 
+    )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_web_app/CalculatorScreen.dart';
+import 'package:my_web_app/SignIn_screen.dart';
+import 'package:my_web_app/Signup_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CalculatorScreen(),
+      home: SignupScreen(),
     );
   }
 }
